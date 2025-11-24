b9fbdc9acee74b07ab5d09e155e77bf8ec758434
blueparrot396-pixel <blueparrot396@gmail.com>
2025-11-25 00:09:38 +0200

    Update LoginValidator.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
@@ -56,8 +56,7 @@ public class LoginValidator extends HttpServlet {
                                    session.setAttribute("userid", rs.getString("id"));
                                    session.setAttribute("user", rs.getString("username"));
                                    session.setAttribute("avatar", rs.getString("avatar"));
-                                   Cookie privilege=new Cookie("privilege","user");
-                                   response.addCookie(privilege);
+
                                    if(request.getParameter("RememberMe")!=null)
                                    {
                                        Cookie username=new Cookie("username",user);
